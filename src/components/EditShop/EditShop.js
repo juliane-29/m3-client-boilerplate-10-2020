9bfb2ddd357c64726a6fba8116b30aea39eacfdf
Juliane <juliane.dominique.huber@gmail.com>
2020-12-12 13:48:30 +0100

    Add EditShop Component

---
+++ src/components/EditShop/EditShop.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class EditShop extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default EditShop
