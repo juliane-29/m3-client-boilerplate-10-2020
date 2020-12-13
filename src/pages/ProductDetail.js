8c49b68920ed20d2b63957f130e36bdea29fed11
Juliane <juliane.dominique.huber@gmail.com>
2020-12-13 20:08:30 +0100

    Check whether user logged in and shop view match to enable user to change ShopDetails or Product Details

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,24 +1,28 @@
 import React, { Component } from "react";
 import axios from "axios";
+import { withAuth } from "./../context/auth-context";
+
 
 class ProductDetail extends Component {
 	state = {
-    brand: "",
-	description: "",
-	price: "",
-	listPrice: "",
-	shippingCost: "",
-	condition: "",
-	category: "",
-	size: "",
-	color: "",
-	material: "",
-	pattern: "",
-	image: "",
-	gender: "",
-	shop: "",
-	user: ""	
-    };
+		brand: "",
+		description: "",
+		price: "",
+		listPrice: "",
+		shippingCost: "",
+		condition: "",
+		category: "",
+		size: "",
+		color: "",
+		material: "",
+		pattern: "",
+		image: "",
+		gender: "",
+		shop: "",
+		user: "",
+		quantity: 1,
+		isDisplayed: false
+	};
 
 	componentDidMount = () => {
 		this.getProductDetails();
@@ -26,41 +30,104 @@ class ProductDetail extends Component {
 
 	getProductDetails = () => {
 		const { id } = this.props.match.params;
-		console.log('id', id)
+		console.log("id", id);
 		axios
 			.get(`http://localhost:5000/api/products/${id}`)
 			.then((response) => {
 				const productInfo = response.data;
-				const { brand, description, price, listPrice, shippingCost, condition, category, size, color, material, pattern, image, gender, shop, user } = productInfo;
-				this.setState({ brand, description, price, listPrice, shippingCost, condition, category, size, color, material, pattern, image, gender, shop, user });
+				const {
+					brand,
+					description,
+					price,
+					listPrice,
+					shippingCost,
+					condition,
+					category,
+					size,
+					color,
+					material,
+					pattern,
+					image,
+					gender,
+					shop,
+					user
+				} = productInfo;
+				this.setState({
+					brand,
+					description,
+					price,
+					listPrice,
+					shippingCost,
+					condition,
+					category,
+					size,
+					color,
+					material,
+					pattern,
+					image,
+					gender,
+					shop,
+					user,
+				});
 			})
 			.catch((err) => {
 				console.log(err);
 			});
 	};
+
+	showForm = () => {
+		this.setState({isDisplayed: !this.state.isDisplayed})
+	}
 	render() {
-		const {brand, description, price, listPrice, shippingCost, condition, category, size, color, material, pattern, image, gender, shop, user} = this.state
-		return (<div>
-		<div className="container">
-		<div>
-		<img src={image} alt="prouct-image" style={{width: "80vw", height: "40vh"}}/>
-		</div>
-        <div className="product-info">
-		<h3>{brand}</h3>
-        <h4>{description}</h4>
-		<p>{listPrice}€</p>
-		<strike>{price}€</strike>
-		<div>
-		<p>{shippingCost}€</p>
-		<p>{condition}</p>
-		<p>{size}</p>
-		<p>{color}</p>
-		</div>
-		<button>Add to Cart</button>
-		</div>
-        </div>
-		</div>)
+		const {
+			brand,
+			description,
+			price,
+			listPrice,
+			shippingCost,
+			condition,
+			category,
+			size,
+			color,
+			material,
+			pattern,
+			image,
+			gender,
+			shop,
+			user,
+			isDisplayed,
+			showForm
+		} = this.state;
+		return (
+			<div>
+				<div className="container">
+					<div>
+						<img
+							src={image}
+							alt="prouct-image"
+							style={{ width: "80vw", height: "40vh" }}
+						/>
+					</div>
+					<div className="product-info">
+						<h3>{brand}</h3>
+						<h4>{description}</h4>
+						<p>{listPrice}€</p>
+						<strike>{price}€</strike>
+						<div>
+							<p>{shippingCost}€</p>
+							<p>{condition}</p>
+							<p>{size}</p>
+							<p>{color}</p>
+						</div>
+						<button>Add to Cart</button>
+						{this.props.user.shop === shop ? <button onClick={this.showForm}>Edit Product</button> : null}
+						{isDisplayed ? <p>Edit Product Component</p> : null}
+
+					</div>
+				</div>
+			</div>
+		);
 	}
 }
 
-export default ProductDetail;
+export default withAuth(ProductDetail);
--- src/pages/ShopDetail.js
+++ src/pages/ShopDetail.js
@@ -1,16 +1,19 @@
 import React, { Component } from "react";
 import axios from "axios";
 import Teaser from "../components/Teaser/Teaser";
-import ProductCard from '../components/ProductCard/ProductCard';
+import ProductCard from "../components/ProductCard/ProductCard";
+import EditShop from "../components/EditShop/EditShop";
 
+import { withAuth } from "./../context/auth-context";
 
 class ShopDetail extends Component {
 	state = {
-        shopName: "",
-        products: "",
+		shopName: "",
+		products: "",
 		shopOwner: "",
 		logo: "",
-		listOfProducts: []
+		listOfProducts: [],
+		isDisplayed: false,
 	};
 
 	componentDidMount = () => {
@@ -18,18 +21,22 @@ class ShopDetail extends Component {
 		this.getAllProducts();
 	};
 
+	showForm = () => {
+		this.setState({ isDisplayed: !this.state.isDisplayed });
+	};
+
 	getShopDetails = () => {
 		const { id } = this.props.match.params;
 		axios
 			.get(`http://localhost:5000/api/shops/${id}`)
 			.then((response) => {
-                console.log('response', response)
+				console.log("response", response);
 				const shopInfo = response.data;
 				const { shopName, products, owner, logo } = shopInfo;
-                console.log("shopInfo", shopInfo);
-                console.log('products', products)
-                console.log('owner', owner)
-				this.setState({ shopName, products, shopOwner:owner, logo });
+				console.log("shopInfo", shopInfo);
+				console.log("products", products);
+				console.log("owner", owner);
+				this.setState({ shopName, products, shopOwner: owner, logo });
 			})
 			.catch((err) => {
 				console.log(err);
@@ -43,7 +50,8 @@ class ShopDetail extends Component {
 	};
 
 	render() {
-        const {shopName, products, shopOwner, logo, listOfProducts } = this.state
+		const { shopName, products, shopOwner, logo, listOfProducts } = this.state;
+		const { id } = this.props.match.params;
 		return (
 			<div>
 				<p>Go Back</p>
@@ -52,23 +60,29 @@ class ShopDetail extends Component {
 					title={shopName}
 					subtitle=""
 				/>
+				
+				{this.props.match.params.id == this.props.user.shop ? (
+					<button onClick={this.showForm}>Edit Shop</button>
+				) : null}
+				{this.state.isDisplayed ? <EditShop /> : null}
+
 				<div className="container">
-				<div class="imageContainer">
-				<img style={{height: "40px"}} src={logo} />
-				</div>
-                <p>{shopOwner.username}</p>
-                <p>Email: {shopOwner.email}</p>
-                <p>Products: {products.length}</p>
+					<div class="imageContainer">
+						<img style={{ height: "40px" }} src={logo} />
+					</div>
+					<p>{shopOwner.username}</p>
+					<p>Email: {shopOwner.email}</p>
+					<p>Products: {products.length}</p>
 				</div>
 				<div className="container">
-              {listOfProducts.map((product, index) => {
-				if(product.shop === shopOwner.shop)
-                return <ProductCard product={product} />
-              })}
-          </div>
+					{listOfProducts.map((product, index) => {
+						if (product.shop === shopOwner.shop)
+							return <ProductCard product={product} />;
+					})}
+				</div>
 			</div>
 		);
 	}
 }
 
-export default ShopDetail;
+export default withAuth(ShopDetail);
